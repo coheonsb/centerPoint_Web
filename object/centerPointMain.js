502524d58f79b7e548b88eae1018f2dd0492e57e
coheonsb <coheonsb@gmail.com>
2018-03-15 00:55:58 +0900

    [수정]
    -페이지 나누기
    -자바스크립트 정리

---
+++ object/centerPointMain.js
@@ -0,0 +1,17 @@
+var C = {};
+C.position = [];
+
+
+function resizeMap(){
+    var mainClass = document.getElementById("mainClass");
+  console.log(mainClass.offsetHeight);
+  document.getElementById("map").style.height = mainClass.offsetHeight +"px";
+}
+
+
+/*
+
+
+
+
+*/
