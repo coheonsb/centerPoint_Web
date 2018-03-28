b9ef2ba78cf18d6152ff1bceed2cc1b126a32039
coheonsb <coheonsb@gmail.com>
2018-03-29 03:00:02 +0900

    [수정,추가]
    -화면 전환 애니메이션 추가
    -각 사용자별 좌표 저장

--- object/centerPointMain.js
+++ object/centerPointMain.js
@@ -5,21 +5,33 @@ C.people = 2;
 C.peopleI
 
 function resizeMap(){
-    var mainClass = document.getElementById("mainClass");
-  document.getElementById("map").style.height = mainClass.offsetHeight +"px";
+    var mainClass = document.getElementById("mainDiv");
+  document.getElementById("map").style.height = mainClass.offsetHeight * 0.87  +"px";
 }
 function optionChoice(){
     
 }
 
 
-function pageControl(tabName,nextTab){
-  var className =tabName.className;
-  tabName.className = className.replace('is-active','');
+function pageControl(nextTab){
   location.href='#'+nextTab.id;
- 
-  
+  anchorControl();
 }
+
+function anchorControl(){
+
+  var url = window.location.href;
+  var slide = url.substring(url.lastIndexOf('#') + 1 + 3);
+  if (slide > -1 && slide < 10)
+    goToSlide(parseInt(slide));
+  else {
+    goToSlide(0);
+  }
+
+}
+
+
+
 
 function peopleCount(count){
   C.people = parseInt(count);
@@ -29,10 +41,14 @@ function peopleCount(count){
 function tab2ListCreate(){
   $("#Tab2_li").empty()
  for(var i=0; i<C.people;i++){
-      $("#Tab2_li").append("<li>"+  (i+1) +"번이용자<button class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' onclick='pageControl(Tab2,Tab3),setLocation("+i+")'>확인</button></li><br>");  
+      $("#Tab2_li").append("<li>"+  (i+1) +"번이용자<button class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' onclick='pageControl(tab3),setLocation("+i+")'>확인</button></li><br>");  
  }
 }
 
 function setLocation(i){
   C.peopleIndex = i;
+}
+
+function goToSlide(number) {
+  $(".carousel").carousel(number);
 }
